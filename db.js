9063592116ab9752e9f0d0b0f308e792b60c8b7d
vvspower <93432189+vvspower@users.noreply.github.com>
2022-12-12 22:36:50 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,6 @@
 require('dotenv').config()
 const mongoose = require("mongoose");
 const mongoURI = process.env.REACT_APP_MONGO_STRING
-// "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
-
-//  mongodb+srv://vvspower:lenovo123@notebookbeta.muuq6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
 const connectToMongo = async () => {
   mongoose.connect(mongoURI, () => {
